b120f952a411512d2662a994be8bffaa04a4fd48
mokssebina <sebina.moks@gmail.com>
2024-05-30 20:55:45 +0200

    submission final

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,12 +7,67 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [list, setList] = useState([])
   // ❗ Create effects to fetch the data and put it in state
+
+  const people = axios.get(urlPeople)  
+  const planets = axios.get(urlPlanets)
+
+  const combineLists = (array1, array2) => {
+
+    let array = []
+
+    for(let i = 0; i < array1.length; i++){
+      let item = array1[i]
+
+      let world = array2.filter((world) => world.id === item.homeworld)
+
+      let object = {
+        id: item.id,
+        name: item.name,
+        homeworld: {
+          id: world[0].id,
+          name: world[0].name
+        }
+      }
+
+      array.push(object)
+    }
+
+    console.log("array: ",array)
+
+    setList(array)
+
+    console.log("list: ", array)
+
+  }
+
+  const getData = () => {
+    axios.all([people, planets])
+    .then(res => {
+
+      combineLists(res[0].data, res[1].data)
+
+    }).catch(err => {
+      console.log("error: ",err)
+    })
+  }
+
+
+  useEffect(() => {
+
+    getData()
+
+  },[])
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {list?.map((item) => (
+        <Character key={item.id} name={item.name} planet={item.homeworld.name} />
+      ))}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character(props) { // ❗ Add the props
+
+  const {name, planet} = props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [show, setShow] = useState(false)  
+
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const showWorld = () => {
+    setShow(!show)
+  }
+
   return (
-    <div>
+    <div className='character-card' onClick={showWorld}>
       {/* Use the same markup with the same attributes as in the mock */}
+      <h3 className='character-name'>{name}</h3>
+      {show?
+        <p>Planet: <span className='character-planet'>{planet}</span></p>
+        :
+        null
+      }
     </div>
   )
 }
